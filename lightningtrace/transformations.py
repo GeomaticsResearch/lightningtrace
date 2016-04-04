a1702e0bfad11f38ec45086affe364b35d0406d6
Michael Ewald <mewald@geomaticsresearch.com>
2016-04-03 19:16:05 -0700

    updating tests and adding numpy functionality

--- lightningtrace/transformations.py
+++ lightningtrace/transformations.py
@@ -1,21 +1,24 @@
 import numpy as np
 
-
 def world_to_pixel_coords(affine, coords):
     """Convert a set of coordinates from world to pixel coordinates.
 
     :param affine: The rasterio affine object
     :param coords: World coordinates you want to translate to pixel coordinates
     """
+    # Convert to numpy array
+    coords = np.array(coords)
+    if coords.shape[0] <= 0 or coords.shape[1] < 2:
+        raise ValueError("Shape of coords is incorrect. Please make sure you have X, Y, optional Z format")
+
     # Affine transformations between raster and world coordinates.
     # See https://github.com/sgillies/affine
     # See https://github.com/mapbox/rasterio/blob/master/docs/windowed-rw.rst
     # See http://www.perrygeo.com/python-affine-transforms.html
     # affine = Convert from pixel coordinates to world coordinates
     reverse_affine = ~affine  # reverse_affine = Convert from world coordinates to pixel coordinates
-
-    col, row = reverse_affine * (coords[0], coords[1])
-    return col, row
+    coords[:, 0:2] = np.apply_along_axis(lambda x: reverse_affine*(x[0], x[1]), axis=1, arr=coords)
+    return coords
 
 
 def pixel_to_world_coords(affine, pixel_coords):
@@ -24,12 +27,14 @@ def pixel_to_world_coords(affine, pixel_coords):
     :param affine: The rasterio affine object
     :param pixel_coords: Pixel coordinates (col, row) you want to translate to world coordinates
     """
+    coords = np.array(pixel_coords)
+    if coords.shape[0] <= 0 or coords.shape[1] < 2:
+        raise ValueError("Shape of pixel_coords is incorrect. Please make sure you have X, Y, optional Z format")
     # Affine transformations between raster and world coordinates.
     # See https://github.com/sgillies/affine
     # See https://github.com/mapbox/rasterio/blob/master/docs/windowed-rw.rst
     # See http://www.perrygeo.com/python-affine-transforms.html
     # affine = Convert from pixel coordinates to world coordinates
     reverse_affine = ~affine  # reverse_affine = Convert from world coordinates to pixel coordinates
-
-    X, Y = affine * (pixel_coords[0], pixel_coords[1])
-    return X, Y
+    coords[:, 0:2] = np.apply_along_axis(lambda x: affine*(x[0], x[1]), axis=1, arr=coords)
+    return coords
--- tests/test_transformations.py
+++ tests/test_transformations.py
@@ -1,4 +1,5 @@
 import pytest
+import numpy as np
 import lightningtrace
 
 
@@ -13,15 +14,27 @@ def dem_rast():
 
 def test_world_to_pixel_transformation(dem_rast):
     """Can we convert world coordinates to pixel coordinates?"""
-    pixel_coordinates = 545.4898088852788, 516.4572418773387
-    world_coords = 299721.936102, 129035.644049
+    world_coords = [(299721.936102, 129035.644049),]
+    pixel_coordinates = [(545.4898088852788, 516.4572418773387),]
     res = lightningtrace.transformations.world_to_pixel_coords(dem_rast.affine, world_coords)
-    assert round(res[0]-pixel_coordinates[0], 5) == 0 and round(res[1]-pixel_coordinates[1], 5) == 0
+    assert round(res[0, 0]-pixel_coordinates[0][0], 5) == 0 and round(res[0, 1]-pixel_coordinates[0][1], 5) == 0
 
 
 def test_pixel_to_world_transformation(dem_rast):
     """Can we convert from pixel to world coordinates?"""
-    pixel_coordinates = 545.4898088852788, 516.4572418773387
-    world_coords = 299721.936102, 129035.644049
+    pixel_coordinates = [(545.4898088852788, 516.4572418773387),]
+    world_coords = [(299721.936102, 129035.644049),]
     res = lightningtrace.transformations.pixel_to_world_coords(dem_rast.affine, pixel_coordinates)
-    assert round(res[0] - world_coords[0], 5) == 0 and round(res[1] - world_coords[1], 5) == 0
+    assert round(res[0, 0] - world_coords[0][0], 5) == 0 and round(res[0, 1] - world_coords[0][1], 5) == 0
+
+
+def test_numpy_world_to_pixel_transformation(dem_rast):
+    """Test our transformations to see if it will consume and successfully translate a numpy array of coordinates"""
+    input_world_coords_np = np.array([
+        (299722.0, 129036.0),
+        (299718.0, 129030.0),
+        (299710.0, 129020.0),
+    ])
+    res = lightningtrace.transformations.world_to_pixel_coords(dem_rast.affine, input_world_coords_np)
+    # TODO: This isn't a real test. Find a better way to do it.
+    assert res.shape == (3,2)
