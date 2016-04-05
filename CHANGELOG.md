d0ad9173152c5465e48fa1a2f4ad2431d5921eb8
Michael Ewald <mewald@geomaticsresearch.com>
2016-04-04 21:30:18 -0700

    getting ready for 0.1.1 dist

---
+++ CHANGELOG.md
@@ -0,0 +1,7 @@
+# CHANGELOG
+
+## version 0.1.1 (released 2016-04-04)
+Fixed a major in the raster subsetting function that incorrectly calculated window extent.
+
+## version 0.1 (released 2016-04-04)
+Initial commit.
--- setup.py
+++ setup.py
@@ -9,11 +9,23 @@ config = {
     'url': 'https://github.com/GeomaticsResearch/lightningtrace',
     'download_url': 'Where to download it.',
     'author_email': 'mewald@geomaticsresearch.com',
-    'version': '0.1',
+    'version': '0.1.1',
     'install_requires': ['pytest', 'rasterio', 'fiona', 'matplotlib', 'shapely'],
     'packages': ['lightningtrace'],
     'scripts': [],
-    'name': 'lightningtrace'
+    'name': 'lightningtrace',
+    'classifiers': [
+                      'Development Status :: 3 - Alpha',
+                      'License :: OSI Approved :: MIT License',
+                      'Topic :: Scientific/Engineering',
+                      'Topic :: Scientific/Engineering :: GIS',
+                      'Topic :: Scientific/Engineering :: Visualization',
+                      'Intended Audience :: Developers',
+                      'Operating System :: MacOS :: MacOS X',
+                      'Operating System :: Microsoft :: Windows',
+                      'Operating System :: POSIX',
+                      'Programming Language :: Python',
+                  ],
 }
 
 setup(**config)
