6b4773764809953cf1f254b952027c0829775de5
Michael Ewald <mewald@geomaticsresearch.com>
2017-07-05 13:09:16 -0700

    updating example

--- examples/subset_raster_and_extract_contours.py
+++ examples/subset_raster_and_extract_contours.py
@@ -12,11 +12,12 @@ with rasterio.open(os.path.join(os.path.dirname(os.path.dirname(__file__)), 'dat
     # Generate the contours
     contours_to_create = [1080, 1070, 1060, 1050, 1040, 1030, 1020, 1010]
     contours = lightningtrace.contour.extract_contours(subset['array'], subset['affine'], contours_to_create)
+    # print(list(contours))
 
     # Create a Geopandas DataFrame from our contours
     df = gp.GeoDataFrame.from_features(contours, rast.crs)
     print(df.head())
-    df.to_file('contours.shp')
+    # df.to_file('contours.shp')
 
     bbox_bounds = 299000, 128000, 300000, 129300
     bbox_geom = shapely.geometry.polygon.Polygon(
@@ -25,5 +26,4 @@ with rasterio.open(os.path.join(os.path.dirname(os.path.dirname(__file__)), 'dat
     bbox_feat = {'geometry': shapely.geometry.mapping(bbox_geom), 'properties': {'fid': 1}}
     df_bbox = gp.GeoDataFrame.from_features([bbox_feat,], crs=rast.crs)
     print(df_bbox.head())
-    df_bbox.to_file('bbox.shp')
-
+    # df_bbox.to_file('bbox.shp')
