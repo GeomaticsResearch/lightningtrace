eb378472fa92de8bf6d2016412ebba82163c0d1c
Michael Ewald <mewald@geomaticsresearch.com>
2017-07-05 14:01:11 -0700

    adding download_url

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ config = {
     'description': 'Quickly contour a digital elevation model using rasterio and matplotlib',
     'author': 'Michael Ewald / GeomaticsResearch LLC',
     'url': 'https://github.com/GeomaticsResearch/lightningtrace',
-    'download_url': 'Where to download it.',
+    'download_url': 'https://github.com/GeomaticsResearch/lightningtrace/archive/0.2.0.tar.gz',
     'author_email': 'mewald@geomaticsresearch.com',
     'version': '0.2.0',
     'install_requires': ['rasterio', 'fiona', 'matplotlib', 'shapely'],
