2c9879695d1507d96056584944c0160e2e3ea8b5
Michael Ewald <mewald@geomaticsresearch.com>
2017-07-06 15:52:14 -0700

    fixing some packaging issues

--- lightningtrace/__init__.py
+++ lightningtrace/__init__.py
@@ -1,4 +1,12 @@
-from lightningtrace import core
-from lightningtrace import transformations
-from lightningtrace import utils
-from lightningtrace import contour
+import shapely.speedups
+if shapely.speedups.available:
+    shapely.speedups.enable()
+import shapely
+import numpy as np
+from matplotlib import _cntr as cntr
+import rasterio
+
+import lightningtrace.core
+import lightningtrace.transformations
+import lightningtrace.utils
+import lightningtrace.contour
--- setup.py
+++ setup.py
@@ -1,35 +1,25 @@
-try:
-    from setuptools import setup
-except ImportError:
-    from distutils.core import setup
-
-config = {
-    'description': 'Quickly contour a digital elevation model using rasterio and matplotlib',
-    'author': 'Michael Ewald / GeomaticsResearch LLC',
-    'url': 'https://github.com/GeomaticsResearch/lightningtrace',
-    'download_url': 'https://github.com/GeomaticsResearch/lightningtrace/archive/0.2.2.tar.gz',
-    'author_email': 'mewald@geomaticsresearch.com',
-    'version': '0.2.2',
-    'install_requires': ['rasterio', 'fiona', 'matplotlib', 'shapely'],
-    'extras_require': {
-        'examples': ['geopandas',],
-        'tests': ['pytest',]
-    },
-    'packages': ['lightningtrace'],
-    'scripts': [],
-    'name': 'lightningtrace',
-    'classifiers': [
-                      'Development Status :: 3 - Alpha',
-                      'License :: OSI Approved :: MIT License',
-                      'Topic :: Scientific/Engineering',
-                      'Topic :: Scientific/Engineering :: GIS',
-                      'Topic :: Scientific/Engineering :: Visualization',
-                      'Intended Audience :: Developers',
-                      'Operating System :: MacOS :: MacOS X',
-                      'Operating System :: Microsoft :: Windows',
-                      'Operating System :: POSIX',
-                      'Programming Language :: Python',
-                  ],
-}
-
-setup(**config)
+from setuptools import setup, find_packages
+setup(
+    name='lightningtrace',
+    description='Quickly contour a digital elevation model using rasterio and matplotlib',
+    author='Michael Ewald / GeomaticsResearch LLC',
+    author_email='mewald@geomaticsresearch.com',
+    url='https://github.com/GeomaticsResearch/lightningtrace',
+    version='0.2.3',
+    install_requires=[
+        'rasterio', 'fiona', 'matplotlib', 'shapely'],
+    license='MIT',
+    packages=find_packages(),
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Topic :: Scientific/Engineering',
+        'Topic :: Scientific/Engineering :: GIS',
+        'Topic :: Scientific/Engineering :: Visualization',
+        'Intended Audience :: Developers',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: POSIX',
+        'Programming Language :: Python',
+    ],
+)
